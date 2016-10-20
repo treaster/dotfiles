d09ddd6e835cdcd6bc4bea841ea377e871c2f0db
Mike Treaster <treaster@gmail.com>
2016-10-19 23:05:30 -0700

    Prompt for git username and email when running bootstrap.sh.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+
+set -eux
+
 rsync -r ~/dotfiles/.bash_profile ~
 rsync -r ~/dotfiles/.gitconfig ~
 rsync -r ~/dotfiles/.vim ~
 rsync -r ~/dotfiles/.vimrc ~
+
+read -p 'git username (e.g. John Smith): ' GIT_USERNAME
+read -p 'git email (e.g. johnsmith@email.com): ' GIT_USEREMAIL
+git config --global user.name "$GIT_USERNAME"
+git config --global user.email "$GIT_USEREMAIL"
