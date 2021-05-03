0d49f02baf9dadb33886920b50ea1b9496a9ff82
Mike Treaster <treaster@gmail.com>
2021-05-03 10:12:28 -0700

    fix some variable handling, esp. when GROOT is unset

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,9 @@
 set -eu
 
 GROOT_ARG=""
-if [ $# -gt 1 ]; then
-    echo "expected at most one argument."
-    echo "usage: $0 [GROOT]"
+if [ $# -gt 3 ]; then
+    echo "expected at most three arguments."
+    echo "usage: $0 [GIT_USERNAME] [GIT_USEREMAIL] [GROOT]"
     exit 1
 fi
 
@@ -13,9 +13,14 @@ if [ $# -eq 1 ]; then
     GROOT_ARG=$1; shift
 fi
 
+# Check if GROOT is entirely unset, and set to empty string if necessary.
+if [ -z "${GROOT+x}" ]; then
+    GROOT=""
+fi
+
 if [ "$GROOT_ARG" == "" ] && [ "$GROOT" == "" ]; then
     # GROOT undefined, and no arg provided. Read from prompt.
-    read -p 'path to monorepo git root ($GROOT): ' GROOT_ARG
+    read -p "path to monorepo git root (\$GROOT) [default=$GROOT]: " GROOT_ARG
 elif [ "$GROOT_ARG" == "" ] && [ "$GROOT" != "" ]; then
     # No arg provided, but GROOT already defined. Just use the same thing again.
     echo 'Inferring $GROOT from existing environment'
@@ -28,18 +33,10 @@ elif [ "$GROOT_ARG" != "" ] && [ "$GROOT" != "$GROOT_ARG" ]; then
     echo "Overriding existing GROOT with new value. (old: $GROOT, new: $GROOT_ARG)"
 fi
 
-GIT_USERNAME=$1; shift
-if [ "$GIT_USERNAME" == "" ]; then
-	read -p 'git username (e.g. John Smith): ' GIT_USERNAME
-fi
-
-GIT_USEREMAIL=$1; shift
-if [ "$GIT_USEREMAIL" == "" ]; then
-	read -p 'git email (e.g. johnsmith@email.com): ' GIT_USEREMAIL
-fi
+read -p 'git email (e.g. johnsmith@email.com. leave empty to skip setting): ' GIT_USEREMAIL
+read -p 'git username (e.g. John Smith. leave empty to skip setting): ' GIT_USERNAME
 
 set -x
-
 SCRIPT_DIR=$(dirname $0)
 rsync -r "${SCRIPT_DIR}/.bashrc" ${HOME}
 rsync -r "${SCRIPT_DIR}/.bash_profile" ${HOME}
@@ -48,5 +45,12 @@ rsync -r "${SCRIPT_DIR}/.vim" ${HOME}
 rsync -r "${SCRIPT_DIR}/.vimrc" ${HOME}
 
 sed -i 's@{{GROOT}}@'"${GROOT_ARG}"'@g' ${HOME}/.bashrc
-git config --global user.name "$GIT_USERNAME"
-git config --global user.email "$GIT_USEREMAIL"
+
+if [ "$GIT_USERNAME" != "" ]; then
+    git config --global user.name "$GIT_USERNAME"
+fi
+if [ "$GIT_USEREMAIL" != "" ]; then
+    git config --global user.email "$GIT_USEREMAIL"
+fi
+
+echo "Done. Now run 'source ~/.bashrc' to refresh your environment."
