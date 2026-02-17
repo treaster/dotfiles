b91b3ba7631ee43ff36fa6edcec7ebf382ad1597
Mike Treaster <treaster@gmail.com>
2026-02-17 13:07:01 -0800

    add some installs to bootstrap.sh; improve ssh config

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,36 @@
 
 set -eu
 
-GROOT_ARG=""
 if [ $# -ne 0 ]; then
     echo "usage: $0"
     exit 1
 fi
+
+# Installs
+sudo apt update
+
+sudo apt install rsync
+sudo apt install gcc
+sudo apt install python3 python-is-python3
+sudo apt install sqlite3
+
+## Docker
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# Terraform
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt-get install terraform
+
 
 read -p 'bootstrap: git email (e.g. johnsmith@email.com. leave empty to skip setting): ' GIT_USEREMAIL
 read -p 'bootstrap: git username (e.g. John Smith. leave empty to skip setting): ' GIT_USERNAME
@@ -35,6 +60,8 @@ if [ "${SSH_KEY_NAME}" != "" ]; then
         -f "${HOME}/.ssh/${SSH_KEY_NAME}"
 
     cat <<EOT >> ~/.ssh/config 
+Host *
+  IdentityFile ~/.ssh/${SSH_KEY_NAME}
 Host github.com
   User git
   Hostname github.com
@@ -45,7 +72,7 @@ fi
 # Configure NetworkManager to use Google DNS servers.
 nmcli connection show
 conn_name="$(nmcli -g NAME connection show --active | head -n 1)"
-nmcli con mod "${conn_name}" ipv4.dns "1.1.1.1 8.8.8.8"
+sudo nmcli con mod "${conn_name}" ipv4.dns "1.1.1.1 8.8.8.8"
 sudo nmcli con mod "${conn_name}" ipv4.dns "1.1.1.1 8.8.8.8"
 sudo nmcli con mod "${conn_name}" ipv4.ignore-auto-dns yes
 sudo nmcli connection modify "${conn_name}" ipv6.dns "2001:4860:4860::8888 2001:4860:4860::8844"
